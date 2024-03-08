317fac4edf6e6f08aedd355b0aa1abbc572745de
Danniel Libor <32063464+blacksmithHub@users.noreply.github.com>
2024-03-08 12:32:14 +0800

    analytics

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -9,6 +9,7 @@ import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
 import VueCookies from 'vue-cookies'
+import VueGtag from 'vue-gtag'
 
 export function registerPlugins (app) {
   app
@@ -16,4 +17,5 @@ export function registerPlugins (app) {
     .use(pinia)
     .use(router)
     .use(VueCookies, { expires: '7d' })
+    .use(VueGtag, { config: { id: 'G-BGEKT2QCSW' } }) // process.env.GOOGLE_ANALYTICS_PROPERTY_ID
 }
